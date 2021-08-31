8af08bc127a31dd1f68f7ffa204dbd936994baf4
Slobodan Novaković <slobodan.novakovic@librafire.com>
2021-08-31 18:04:38 +0200

    Finish one page for desktop

--- theme-template/js/modules/slidetoggle.js
+++ theme-template/js/modules/slidetoggle.js
@@ -1,3 +1,4 @@
+/*Lessons items toglle slide down*/
 var slideOpen = true;
 var heightChecked = false;
 var initHeight = 0;
@@ -7,10 +8,10 @@ function slideToggle() {
 
     window.clearInterval(intval);
 
-    var toggleArrow = document.getElementById("mbtn");
+    var toggleArrow = document.getElementById("toggle-btn");
     toggleArrow.classList.toggle("arrow-switch");
 
-    var mdiv = document.getElementById('mdiv');
+    var mdiv = document.getElementById('toggle-element');
     mdiv.classList.toggle("overflow-rule");
 
     if(!heightChecked) {
@@ -25,7 +26,7 @@ function slideToggle() {
 			mdiv.style.height = h + 'px';
 			if(h <= 0)
 				window.clearInterval(intval);
-			}, 1
+			}, 0.001
 		);
     }
     else {
@@ -36,7 +37,52 @@ function slideToggle() {
 			mdiv.style.height = h + 'px';
 			if(h >= initHeight)
 				window.clearInterval(intval);
-			}, 1
+			}, 0.001
 		);
     }
+}
+
+
+/*Classes toggle slide above the toggle triger*/
+var linkToggle = document.querySelectorAll('.classes-show-more-toggle');
+
+for(i = 0; i < linkToggle.length; i++){
+
+  linkToggle[i].addEventListener('click', function(event){
+
+    event.preventDefault();
+
+    var container = document.getElementById(this.dataset.container);
+
+    if (!container.classList.contains('active')) {
+      
+      
+      container.classList.add('active');
+      container.style.height = 'auto';
+
+      var height = container.clientHeight + 'px';
+
+      container.style.height = '0px';
+
+      setTimeout(function () {
+        // linkToggle.innerHTML = "Swapped text!";
+        container.style.height = height;
+      }, 0);
+      
+    } else {
+      
+      container.style.height = '0px';
+      
+      container.addEventListener('transitionend', function () {
+        // linkToggle.innerHTML = "Show more";
+        container.classList.remove('active');
+      }, {
+        once: true
+      });
+      
+    }
+    event.target.textContent == 'Show more' ? event.target.textContent = 'Show less' : event.target.textContent = 'Show more';
+    
+  });
+
 }
